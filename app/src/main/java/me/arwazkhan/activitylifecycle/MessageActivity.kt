440e11aef3650ae79aacdc00ab7bb9eba36caa69
PranavVyas20 <Pranavvyas2003@gmail.com>
2021-09-14 00:41:07 +0530

    View binding and layout changes

--- app/src/main/java/me/arwazkhan/activitylifecycle/MessageActivity.kt
+++ app/src/main/java/me/arwazkhan/activitylifecycle/MessageActivity.kt
@@ -3,14 +3,15 @@ package me.arwazkhan.activitylifecycle
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import me.arwazkhan.activitylifecycle.databinding.ActivityMessageBinding
 
 class MessageActivity : AppCompatActivity() {
+//    lateinit var binding2: ActivityMessageBinding
     lateinit var txtMessage:TextView
     var message = "Custom Message"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message)
-
 
         txtMessage= findViewById(R.id.txtMessage)
 
